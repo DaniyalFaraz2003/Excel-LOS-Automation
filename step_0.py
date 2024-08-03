12b787dc0ee089aa5ed18756466cd6abd371bb9e
Mustafa Faraz <mustafafaraz87@gmail.com>
2024-08-03 23:35:10 +0500

    found length of records

--- step_0.py
+++ step_0.py
@@ -1,4 +1,14 @@
 import openpyxl
 
 def step_0():
-    pass
+    wb = openpyxl.load_workbook("./Example0_LOS Original.xlsx")
+    ws = wb.active # active worksheet
+    # extract all names from first column to find total number of records
+    first_column = [cell.value for cell in ws['A'][1:]]
+    names = list({k: None for k in first_column}.keys())
+
+    number_of_records = len(names)
+    
+
+
+    wb.save("./bot_outputs/step_0.xlsx")
