38a68eae2fd6f4032b9f4a5b4301bd26cd065828
Mustafa Faraz <mustafafaraz87@gmail.com>
2024-08-04 00:14:53 +0500

    extracted number of records

--- step_0.py
+++ step_0.py
@@ -1,7 +1,7 @@
 import openpyxl
 
 def step_0():
-    wb = openpyxl.load_workbook("./Example0_LOS Original.xlsx")
+    wb = openpyxl.load_workbook("./bot_outputs/step_0.xlsx")
     ws = wb.active # active worksheet
     # extract all names from first column to find total number of records
     first_column = [cell.value for cell in ws['A'][1:]]
@@ -9,6 +9,59 @@ def step_0():
 
     number_of_records = len(names)
     
+    ALL_RECORDS = {
+        "Volumes": [
+            "Oil Sales - Bbls",
+            "Gas Sales - mcf",
+            "NGL Sales - Bbls",
+            "NGL Sales - Gal"
+        ],
+        "Revenue": [
+            "Oil Sales Rev",
+            "Gas Sales Rev",
+            "NGL Sales Rev",
+            "Oil Rev Deduct",
+            "Gas Rev Deduct",
+            "NGL Rev Deduct"
+        ],
+        "Operating Expenses": [
+            "Severance Taxes",
+            "Other Deductions",
+            "Chemicals",
+            "Communications",
+            "Consulting",
+            "Contract Labor",
+            "Fuel & Power",
+            "Hot Oil & Other Treatments",
+            "Insurance",
+            "Legal",
+            "Marketing",
+            "Measurement/Metering",
+            "Miscellaneous",
+            "Overhead",
+            "Professional Services",
+            "Pumping & Gauging",
+            "Rental Equipment",
+            "Repairs & Maintenance",
+            "Road & Lease Maintenance",
+            "Salt Water Disposal",
+            "Supervision",
+            "Supplies",
+            "Ad Valorem",
+            "Trucking & Hauling",
+            "Vacuum Truck/Clean Up",
+            "Well Servicing",
+            "Workover Rig",
+            "Gathering & Transport Chg",
+            "Swd Disposal Chg",
+            "Total Expenses",
+            "Net Operating Profit"
+        ]
+    }
+
+    
 
 
     wb.save("./bot_outputs/step_0.xlsx")
+
+step_0()
